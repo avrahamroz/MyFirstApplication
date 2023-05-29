2943091db2096ddc0540dc47b7a57cac73d6df0f
avrahamroz <7658630@gmail.com>
2023-05-29 22:10:42 +0300

    My first application

---
+++ app/src/main/java/com/example/rozaplications/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.rozaplications
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import java.util.logging.Level
+import java.util.logging.Logger
+
+class MainActivity : ComponentActivity() {
+    var logger = Logger.getLogger("MainActivity")
+
+    lateinit var editText: EditText
+    lateinit var EditEmail:EditText
+    lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btn = findViewById<Button>(R.id.Button)
+        btn.setOnClickListener({view ->
+            logger.log(Level.INFO, findViewById<TextView>(R.id.editText).text.toString())
+            logger.log(Level.INFO, findViewById<TextView>(R.id.editEmail).text.toString())
+        })
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logger.log(Level.INFO, "OnStopMethod")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logger.log(Level.INFO, "OnStopMethod")
+    }
+}
+
